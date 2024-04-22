e4e8812662802e9ce81b2e53d5ad18ec85fd88d0
Sumshi <sumayasomow@gmail.com>
2024-04-22 20:35:25 +0300

    🔥 Backend completed
    backend completed

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,24 @@ import mongoose from 'mongoose';
 // import the book model
 import { Book } from './models/bookModel.js';
 import  booksRoute  from './routes/booksRoute.js'
- 
+
+import cors from 'cors';
+
 // create an express app
 const app = express();
 
-// middleware to parse json data
+// middleware to parse json data(request body)
 app.use(express.json());
 
+// middleware for handling cors POLICY
+// app.use(cors());
+
+// // allowing custom origins with cors
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 
 // define routes
 app.get('/', (request, response) => {
@@ -25,8 +36,8 @@ app.use('/books', booksRoute);
 
 
 mongoose.connect(mongoDBURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
   serverSelectionTimeoutMS: 5000
 }).then(() => {
   console.log('App connected to database');
