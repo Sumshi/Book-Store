2839c0e3e66e0d0b2125da6e5b782ac6d262cc53
Sumshi <sumayasomow@gmail.com>
2024-04-23 23:44:10 +0300

    🐛 Made file public
    made file public

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+'mongodb+srv://sumayasomow:7N7CPfFjh2MRDsne@bookstore.17rifnm.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstore';
